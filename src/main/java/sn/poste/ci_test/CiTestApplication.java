9eaf0ef97647332787068179a88159895ae46077
mbayesenghor <30715102+mbayesenghor@users.noreply.github.com>
2020-09-03 07:45:04 +0000

    Initial commit

---
+++ src/main/java/sn/poste/ci_test/CiTestApplication.java
@@ -0,0 +1,13 @@
+package sn.poste.ci_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sn/poste/ci_test/CiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.poste.ci_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
